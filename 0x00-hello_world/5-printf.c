774cb8a5c39a973b110c5330ceeff7cda665d64e
me996 <mariam.bouakrab@etu.uae.ac.ma>
2023-07-13 17:34:22 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <sstdio.h>
+/**
+ * main - Entry point 
+ * return: always (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
