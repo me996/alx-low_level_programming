3e86a6d5d46a0a095ee448f34a4e28e282c0109b
me996 <mariam.bouakrab@etu.uae.ac.ma>
2023-07-15 14:27:19 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - A program that prints a line with puts function 
  * return: always (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
 
