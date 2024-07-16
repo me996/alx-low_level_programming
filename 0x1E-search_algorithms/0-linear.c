918c7ee075dc237bac86744be8726bffbcf19aae
me996 <me996@gmail.com>
2024-07-16 15:44:44 +0100

    linear_search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,7 +18,7 @@ return (-1);
 }
 for (i = 0; i < size; i++)
 {
-printf("comparing %d to %d\n", array[i], value);
+printf("Value checked array[%d] = [%d]\n", array[i], value);
 if (array[i] == value)
 {
 return (i);
