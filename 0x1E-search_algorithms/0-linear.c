9879c24359feb300336637768271eb2691a96d78
me996 <me996@gmail.com>
2024-07-16 15:19:47 +0100

    linear_serach

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,11 +1,11 @@
 #include "search_algos.h"
 
 int linear_search(int *array, size_t size, int value) {
+size_t i;
 if (array == NULL){
     return -1;
 }
-
-for (size_t i = 0; i < size; ++i) {
+for (i = 0; i < size; i++) {
 printf("comparing %d to %d\n", array[i], value);
 if (array[i] == value) {
     return i;
