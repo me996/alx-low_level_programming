31afd1bb092ec672627a2a2a7f562514e0753f2c
me996 <me996@gmail.com>
2024-07-16 22:01:32 +0100

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,15 +11,15 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-size_t i;
+unsigned int i;
 
 if (array == NULL)
 {
 return (-1);
 }
-for (i = 0; i < size; i++)
+for (i = 0; array && i < size; i++)
 {
-printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+printf("Value checked array[%d] = [%d]\n", array[i], value);
 if (array[i] == value)
 {
 return (i);
