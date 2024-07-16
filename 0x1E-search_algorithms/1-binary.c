63d6889624b44150d30818107a72ba2ebe1b3746
me996 <me996@gmail.com>
2024-07-16 16:06:52 +0100

    time_cmp

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+/**
+ * linear_search - Search for a value in an array.
+ * @array: Pointer to the first element of the array to search.
+ * @size: Number of elements in the array.
+ * @value: Value to search for in the array.
+ *
+ * Return: The index of the first occurrence of value in array,
+ * or -1 if value is not present or if array is NULL.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+if (array == NULL)
+return (-1);
+}
--- 0x1E-search_algorithms/README.md
+++ 0x1E-search_algorithms/README.md
@@ -1,2 +1,3 @@
 # 0x1E. C - Search Algorithms
 
+# linear_search - Search for a value in an array using linear serach
